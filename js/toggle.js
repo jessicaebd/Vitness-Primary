59c9a024af1b6f275328c2dca04488e3531d89b4
raynardtjhang <raytjhang@gmail.com>
2022-01-21 19:53:03 +0700

    fix toggle

--- js/toggle.js
+++ js/toggle.js
@@ -2,6 +2,11 @@ let toggle = document.getElementById('hamburger');
 let menu = document.getElementById('miniNavbar');
 let body = document.body;
 toggle.onclick = function () {
+	// prevent null read
+	if (menu.style.display === '') {
+		menu.style.display = 'none';
+	}
+	// check condition
 	if (menu.style.display === 'none') {
 		menu.style.display = 'block';
 		body.style.overflow = 'hidden';
