13f2eed3a51975e026a17a5f63905bbe0b6fddab
raynardtjhang <raytjhang@gmail.com>
2022-02-25 21:41:21 +0700

    screensaver

--- js/screensaver.js
+++ js/screensaver.js
@@ -1,21 +1,18 @@
 var canvas = document.querySelector('canvas');
 var ctx = canvas.getContext('2d');
-
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-// Variables
 let x = 0;
 let y = 0;
 let movex = 2;
 let movey = 2;
 let size = 300;
+let logo;
 
-// Make img
-base_image = new Image();
-base_image.src = './assets/logo.png';
-base_image.onload = function () {
-	ctx.imageSmoothingEnabled = false;
+logo = new Image();
+logo.src = './assets/logo.png';
+logo.onload = function () {
 	setInterval(animate, 10);
 };
 
@@ -29,17 +26,15 @@ function animate() {
 	}
 	x += movex;
 	y += movey;
-	ctx.drawImage(base_image, x, y, size, size);
+	ctx.drawImage(logo, x, y, size, size);
 }
 
-// Logic screen saver
 function screensaver() {
 	$('canvas').show();
 	document.body.style.overflow = 'hidden';
 }
 
 let counter;
-
 window.addEventListener('mousemove', function (e) {
 	$('canvas').hide();
 	this.clearTimeout(counter);
