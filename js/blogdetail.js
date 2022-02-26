9060bdf57b339d6cb96e08802be47e6c222f1f67
raynardtjhang <raytjhang@gmail.com>
2022-02-26 18:04:27 +0700

    save

--- js/blogdetail.js
+++ js/blogdetail.js
@@ -2,8 +2,6 @@ $(document).ready(function () {
 	let url = new URL(window.location.href);
 	let blog_id = url.searchParams.get('id');
 
-	let blogDetailContainer = $('#blogDetailContainer');
-
 	$.getJSON('../posts.json', function (data) {
 		let blogs = data;
 		let blog = data.find(({ id }) => id == blog_id);
@@ -28,19 +26,5 @@ $(document).ready(function () {
       `);
 			}
 		});
-
-		// blogDetailContainer.append(`
-		//   <p class="page-title">${blog.title}</p>
-		//   <p class="blog-date">Published on ${blog.date}</p>
-		//   <p class="blog-author">By ${blog.author}</p>
-		//   <img src="${blog.image}" class="blog-image">
-
-		//   <br><br>
-		//   <h2 class="blog-category">${blog.category}</h2>
-		// <br>
-		//   <p class="blog-desc">${blog.description}</p>`);
 	});
-
-	let searchParam = new URLSearchParams(location.search);
-	let id = searchParam.get('id');
 });
