19357642b8bcc206b51419317ad723719205b9d7
raynardtjhang <raytjhang@gmail.com>
2022-02-27 21:14:46 +0700

    save

--- js/blog.js
+++ js/blog.js
@@ -14,14 +14,16 @@ $(document).ready(function () {
         </a>`);
 			} else {
 				blogContainer.append(`
-        <div class="col-lg-3 col-md-4 col-sm-6">
+        <div class="col-lg-4 col-md-6">
           <a href="blog-detail.html?id=${blog.id}">
             <div class="blog-card">
               <img src="${blog.image}" class="blog-card-image">
-              <div class="blog-card-desc">
-                <p class="blog-card-title">${blog.title}</p>
+              <div class="blog-card-text">
+                
                 <p class="class-card-capsule-black">${blog.category}</p>
                 <p class="blog-card-date">${blog.date}</p>
+                <p class="blog-card-title">${blog.title}</p>
+                <p class="blog-card-description">"${blog.description}"</p>
               </div>
             </div>
           </a>
